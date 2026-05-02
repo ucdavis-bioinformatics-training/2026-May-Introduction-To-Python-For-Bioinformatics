5ce9a94553556040e26e997e1a36d45fca5967f5
Nikhil Joshi <najoshi@ucdavis.edu>
2026-05-01 18:09:24 -0700

    initial commit

---
+++ python/scripts/fastq_trimmer.py
@@ -0,0 +1,30 @@
+def trim(qual,threshold):
+    pos=0
+    for base in qual:
+        if (ord(base)-33 < threshold):
+            return(pos)
+        else:
+            pos += 1
+    return(pos)
+
+
+out = open("trimmed.fastq","w")
+fq = open("samp1.fastq","r")
+
+while (True):
+
+    header = fq.readline().rstrip("\n")
+    if not header:
+        break
+    seq = fq.readline().rstrip("\n")
+    header2 = fq.readline().rstrip("\n")
+    qual = fq.readline().rstrip("\n")
+
+    trimpos = trim(qual,15)
+    seq = seq[0:trimpos]
+    qual = qual[0:trimpos]
+
+    out.write(header+"\n"+seq+"\n"+header2+"\n"+qual+"\n")
+
+fq.close()
+out.close()
